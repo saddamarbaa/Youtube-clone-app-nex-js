e95a11204dc91878a2801c53c5f8c577cce6a9a4
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-10-05 17:54:57 +0700

    feat: add user authentication

--- next.config.js
+++ next.config.js
@@ -1,3 +1,34 @@
-module.exports = {
-  reactStrictMode: true,
-}
+/** @format */
+
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+	if (phase === PHASE_DEVELOPMENT_SERVER) {
+		return {
+			reactStrictMode: true,
+			env: {
+				API_KEY: process.env.API_KEY,
+				AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+				PROJECT_ID: process.env.PROJECT_ID,
+				STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+				MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+				APP_ID: process.env.APP_ID,
+			},
+			images: {
+				domains: ["https://lh3.googleusercontent.com"],
+			},
+		};
+	}
+
+	return {
+		reactStrictMode: true,
+		env: {
+			API_KEY: process.env.API_KEY,
+			AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+			PROJECT_ID: process.env.PROJECT_ID,
+			STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+			MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+			APP_ID: process.env.APP_ID,
+		},
+	};
+};
